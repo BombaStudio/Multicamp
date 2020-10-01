aeac2135fb0c234c5d81cd362fec24a13a445da7
BombaStudio <bombaoyun111@gmail.com>
2020-10-02 00:14:35 +0300

    defr

--- app/src/main/java/com/giant/liders/FirstFragment.kt
+++ app/src/main/java/com/giant/liders/FirstFragment.kt
@@ -1,8 +1,11 @@
 package com.giant.liders
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.giant.liders.data.LidersApi
 import com.giant.liders.data.RetrofitProvider
 import com.giant.liders.data.model.Article
@@ -11,19 +14,16 @@ import kotlinx.coroutines.launch
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launch {
             val response: ArticlesResponse = RetrofitProvider.lidersApi.ArticlesResponse()
             rev.adapter = ArticlesAdapter(response.articles.orEmpty().toMutableList()){
-                /*
                 rev.adapter = ArticlesAdapter(response.articles.orEmpty().toMutableList()) {
-                    val direction = FirstFragmentDirections.actionNewsFragmentToArticleFragment(it)
+                    val direction = NewsFragmentDirections.actionFirstFragmentToNewFragment(it)
                     findNavController().navigate(direction)
                 }
 
-                 */
             }
         }
     }
--- app/src/main/java/com/giant/liders/NewFragment.kt
+++ app/src/main/java/com/giant/liders/NewFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_new.*
+import kotlinx.android.synthetic.main.item_article.view.*
 
 
 class NewFragment : Fragment(R.layout.fragment_new) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private val args by navArgs<NewFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageViewNew.imageBanner.load(args.article.banner)
+        imageView2.imageBanner.load(args.article.leaders.photo)
+        textViewLeader.text = args.article.description
+        textView3.text = args.article.leaders.name
     }
 }
